411a707290253a7384823052c029be17aea006e4
Aliaksei.Haladko <aliaksei.haladko@ventionteams.com>
2025-08-25 16:55:02 +0300

    add goal to input query params

--- src/constants.ts
+++ src/constants.ts
@@ -2,6 +2,7 @@ export const enum InputQueryParamams {
     yearlyPrice = "yearly_price",
     yearlyMonthPrice = "yearly_price_per_month",
     monthlyPrice = "monthly_price",
+    goal = "goal",
 }
 
 export const enum OutputQueryParams {
