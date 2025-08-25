117b41f154b15ac52120de84d767248ff886ce12
Aliaksei.Haladko <aliaksei.haladko@ventionteams.com>
2025-08-25 18:19:53 +0300

    add on skip handler

--- src/components/Video/index.tsx
+++ src/components/Video/index.tsx
@@ -1,8 +1,16 @@
 import styles from "./video.module.css";
+import { useNavigate } from "react-router-dom";
 
 export const Video = () => {
+    const navigate = useNavigate();
+
+    const onSkipHandler = () => {
+        navigate("/skip");
+    }
+
     return (
         <section className={styles.videoWrapper}>
+           <button className={styles.skip} onClick={onSkipHandler}>Skip</button>
            <div className={styles.img}></div>
         </section>
     )
